9ecd5da0a1f651526dadea152f60b7c4e64af275
jespinal899 <jespinalmontenegro@gmail.com>
2025-02-28 15:53:43 -0600

    Update logica.js

--- logica.js
+++ logica.js
@@ -1,8 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const menuBar = document.querySelector(".menuBar");
-    
-    menuToggle.addEventListener("click", function () {
-        menuBar.classList.toggle("active");
-    });
-});
+
+
+function toggleMenu() {
+    document.getElementById("menuBar").classList.toggle("active");
+}
+
+const imagenes = ["imagenes/set1.png", "imagenes/set2.png", "imagenes/set3.png"];
+let indice = 0;
+
+function cambiarImagen(direccion) {
+    indice += direccion;
+    if (indice < 0) indice = imagenes.length - 1;
+    if (indice >= imagenes.length) indice = 0;
+    document.getElementById("productoImagen").src = imagenes[indice];
+}
